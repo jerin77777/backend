b225d1f5a2a217d946b501c8c6111928bc787dc2
jerin77777 <jerinjacob0007@gmail.com>
2024-10-31 19:19:43 +0530

    backup

---
+++ gen.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import os 
+
+
+
+def gen(query):
+
+    client = OpenAI(
+        api_key = os.getenv("key") 
+    )
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "user", "content": query}],
+        stream=False,
+    )
+
+    result =  response.choices[0].message.content.replace("```json","").replace("```","")
+    return result
+
